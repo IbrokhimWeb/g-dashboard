06b97490359bdfba3b8a5b0498de542538fa9aea
Nodir Xaydarkulov <xaydarkulov.n@astrum.domain>
2023-03-01 16:21:54 +0500

    Adg

--- src/Products/components/ProductTypeAdd.js
+++ src/Products/components/ProductTypeAdd.js
@@ -1,121 +1,126 @@
 import {
-    Box,
-    Button,
-    Card,
-    Checkbox,
-    FormControl,
-    InputLabel,
-    makeStyles,
-    MenuItem,
-    Select,
-    TextField,
-  } from "@material-ui/core";
-  import { Backlink } from "@saleor/macaw-ui";
-  import React, { useEffect, useState } from "react";
-  import { useNavigate } from "react-router-dom";
-  import CardSpacer from "../../components/CardSpacer";
-  import Container from "../../components/Container";
-  import PageHeader from "../../components/PageHeader";
-  import $host from "../../http";
-  import CardTitle from "../../components/CardTitle";
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+  TextField,
+} from "@material-ui/core";
+import { Backlink } from "@saleor/macaw-ui";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CardSpacer from "../../components/CardSpacer";
+import Container from "../../components/Container";
+import PageHeader from "../../components/PageHeader";
+import $host from "../../http";
+import CardTitle from "../../components/CardTitle";
 import FormSpacer from "../../components/FormSpacer/FormSpacer";
 import { formToJSON } from "axios";
-  
-  const useStyles = makeStyles((theme) => ({
-    mainCardInfo: {
-      paddingTop: 0,
-      padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
-    },
-  }));
-  
-  const ProductTypeAdd = (props) => {
-    const navigate = useNavigate();
-    const [newData, setNewData] = useState({
-        name: "",
-        product_type_attribute: 0,
-    });
-    console.log(newData);
-    const [productsTypeAttribute, setProductsTypeAttribute] = useState(null);
-    console.log(productsTypeAttribute);
-    const [isProductsTypeAttribute, setIsProductsTypeAttribute] = useState(newData?.product_type_attribute);
-    const classes = useStyles(props);
-  
-      const handleSubmit = async () => {
-        const formData = new FormData();
-        formData.append('name', newData.name);
-        formData.append('product_type_attribute', newData.product_type_attribute);
-      const res = await $host.post(`/dashboard/product-type/`, 
-    //   {
-    //     product_type_attributes: {
-    //       name: newData.product_type_attribute,
-    //     },
-    //   }
-    formData);
-      res?.statusText ? navigate("/product-type") : alert("Nimadir hato ketdi");
-    };
-  
-    useEffect(() => {
-      $host
-        .get("dashboard/product-type-attribute/")
-        .then((res) => setProductsTypeAttribute(res.data.results))
-        .catch((error) => console.error(error));
-    }, []);
 
-    return (
-      <Container>
-        <Backlink onClick={() => navigate("/product-type")}>Тип продуктов</Backlink>
-        <PageHeader title="Создать новый тип продукта" />
-        <div>
+const useStyles = makeStyles((theme) => ({
+  mainCardInfo: {
+    paddingTop: 0,
+    padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
+  },
+}));
+
+const ProductTypeAdd = (props) => {
+  const navigate = useNavigate();
+  const [newData, setNewData] = useState({
+    name: "",
+    product_type_attribute: 0,
+  });
+  console.log(newData);
+  const [productsTypeAttribute, setProductsTypeAttribute] = useState(null);
+  console.log(productsTypeAttribute);
+  const [isProductsTypeAttribute, setIsProductsTypeAttribute] = useState(
+    newData?.product_type_attribute
+  );
+  const classes = useStyles(props);
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("name", newData.name);
+    formData.append("product_type_attribute", newData.product_type_attribute);
+    const res = await $host.post(
+      `/dashboard/product-type/`,
+      //   {
+      //     product_type_attributes: {
+      //       name: newData.product_type_attribute,
+      //     },
+      //   }
+      formData
+    );
+    res?.statusText ? navigate("/product-type") : alert("Nimadir hato ketdi");
+  };
+
+  useEffect(() => {
+    $host
+      .get("dashboard/product-type-attribute/")
+      .then((res) => setProductsTypeAttribute(res.data.results))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <Container>
+      <Backlink onClick={() => navigate("/product-type")}>
+        Тип продуктов
+      </Backlink>
+      <PageHeader title="Создать новый тип продукта" />
+      <div>
         <Card>
-        <CardTitle title={"Основная информация"} />
-                    <div className={classes.mainCardInfo}>
-                        <TextField 
-                        fullWidth 
-                        placeholder={"Название"} 
-                        name="name" 
-                        value={newData?.name} 
-                        onChange={(e) => setNewData(prev => ({ ...prev, name: e.target.value }))} />
-                        <CardSpacer />
-          <FormControl fullWidth>
-                <InputLabel id="demo-simple-gh-label">Тип аттрибутов продукта</InputLabel>
-                <Select
+          <CardTitle title={"Основная информация"} />
+          <div className={classes.mainCardInfo}>
+            <TextField
+              fullWidth
+              placeholder={"Название"}
+              name="name"
+              value={newData?.name}
+              onChange={(e) =>
+                setNewData((prev) => ({ ...prev, name: e.target.value }))
+              }
+            />
+            <CardSpacer />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-gh-label">
+                Тип аттрибутов продукта
+              </InputLabel>
+              <Select
                 labelId="demo-simple-gh-label"
                 id="demo-simple-select"
                 value={isProductsTypeAttribute}
-                onChange={e => {
-                    setIsProductsTypeAttribute(e.target.value);
-                    setNewData(prev => ({
-                    ...prev, product_type_attribute
-                         : e.target.value
-                                }))
-                                }}
-                                >
-                                    {
-                    productsTypeAttribute?.map((name) => (
-                      <MenuItem 
-                        key={name.id} 
-                        value={name.id}>
-                          {name.name}
-                        </MenuItem>
-                    ))
-                  }   
-
-                  </Select>
+                onChange={(e) => {
+                  setIsProductsTypeAttribute(e.target.value);
+                  setNewData((prev) => ({
+                    ...prev,
+                    product_type_attribute: e.target.value,
+                  }));
+                }}
+              >
+                {productsTypeAttribute?.map((name) => (
+                  <MenuItem key={name.id} value={name.id}>
+                    {name.name}
+                  </MenuItem>
+                ))}
+              </Select>
             </FormControl>
             <FormSpacer />
-            </div>
-            </Card>
-          <Button
-            style={{ float: "right", marginTop: "10px", padding: "10px 70px" }}
-            variant="contained"
-            onClick={handleSubmit}
-          >
-            Save
-          </Button>
-        </div>
-      </Container>
-    );
-  };
-  
-  export default ProductTypeAdd;
-  
+          </div>
+        </Card>
+        <Button
+          style={{ float: "right", marginTop: "10px", padding: "10px 70px" }}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Save
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default ProductTypeAdd;
--- src/pages/ProductDetailPage/ProductDetailsPageEdit.js
+++ src/pages/ProductDetailPage/ProductDetailsPageEdit.js
@@ -1,4 +1,15 @@
-import { Box, Button, Card, Checkbox, FormControl, InputLabel, makeStyles, MenuItem, Select, TextField } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+  TextField,
+} from "@material-ui/core";
 import { Backlink } from "@saleor/macaw-ui";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -10,98 +21,116 @@ import CardTitle from "../../components/CardTitle";
 import FormSpacer from "../../components/FormSpacer/FormSpacer";
 
 const useStyles = makeStyles((theme) => ({
-    mainCardInfo: {
-        paddingTop: 0,
-        padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
-    },
+  mainCardInfo: {
+    paddingTop: 0,
+    padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
+  },
 }));
 
-
 const ProductDetailsPageEdit = (props) => {
-    const params = useParams()
-    const navigate = useNavigate();
-    const [newData, setNewData] = useState(null);
-    console.log(newData);
-    const [attributes, setAttribute] = useState(null);
-    const [inventors, setInventors] = useState(null);
-    const [isCategories, setIsCategories] = useState(newData?.category);
-    const classes = useStyles(props);
+  const params = useParams();
+  const navigate = useNavigate();
+  const [newData, setNewData] = useState(null);
+  console.log(newData);
+  const [attributes, setAttribute] = useState(null);
+  const [inventors, setInventors] = useState(null);
+  const [isCategories, setIsCategories] = useState(newData?.category);
+  const classes = useStyles(props);
 
-    const handleSubmit = async () => {
-        const res = await $host.put(`/dashboard/product-attribute-values/${params.id}/`, newData);
-        res?.statusText ? navigate("/product-attribute-values") : alert("Nimadir hato ketdi");
-    };
-
-    useEffect(() => {
-        $host.get("dashboard/product-attribute-value/")
-          .then((res) => setAttribute(res.data.results))
-          .catch((error) => console.error(error))
-      }, []);
-            
-    return (
-        <Container>
-            <Backlink onClick={() => navigate("/product-attribute-values")}>Атрибут продукта 2</Backlink>
-            <PageHeader title="Создать новый атрибут продукта" />
-            <div>
-                <Card>
-                    <CardTitle title={"Основная информация"} />
-                    <div className={classes.mainCardInfo}>
-                        <Box sx={{ minWidth: 120 }}>
-                        <FormControl fullWidth>
-                                <InputLabel id="demo-simple-gh-label">Атрибут продукта</InputLabel>
-                                <Select
-                                    labelId="demo-simple-gh-label"
-                                    id="demo-simple-select"
-                                    value={isCategories}
-                                    onChange={e => {
-                                        setIsCategories(e.target.value);
-                                        setNewData(prev => ({
-                                            ...prev, productinventory
-                                                : e.target.value
-                                        }))
-                                    }}
-                                >
-                                    {
-                                        inventors?.map(({ sku, id }) => (<MenuItem key={id} value={id}>{sku}</MenuItem>))
-                                    }   
-
-                                </Select>
-                            </FormControl>
-                            <FormSpacer />
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-gh-label">Тип продукта</InputLabel>
-                                <Select
-                                    labelId="demo-simple-gh-label"
-                                    id="demo-simple-select"
-                                    value={isCategories}
-                                    onChange={e => {
-                                        setIsCategories(e.target.value);
-                                        setNewData(prev => ({
-                                            ...prev, attributevalues
-                                                : e.target.value
-                                        }))
-                                    }}
-                                >
-                                    {
-                                        attributes?.map(({ attribute_value, id }) => (<MenuItem key={id} value={id}>{attribute_value}</MenuItem>))
-                                    }   
-
-                                </Select>
-                            </FormControl>
-                        </Box>
-                    </div>
-                </Card>
-                <CardSpacer />
-                <Button
-                    style={{ float: "right", marginTop: "10px", padding: "10px 70px" }}
-                    variant="contained"
-                    onClick={handleSubmit}
-                >
-                    Сохранить
-                </Button>
-            </div>
-        </Container>
+  const handleSubmit = async () => {
+    const res = await $host.put(
+      `/dashboard/product-attribute-values/${params.id}/`,
+      newData
     );
+    res?.statusText
+      ? navigate("/product-attribute-values")
+      : alert("Nimadir hato ketdi");
+  };
+
+  useEffect(() => {
+    $host
+      .get("dashboard/product-attribute-value/")
+      .then((res) => setAttribute(res.data.results))
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    $host
+      .get("dashboard/product-inventors/")
+      .then((res) => setInventors(res.data.results))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <Container>
+      <Backlink onClick={() => navigate("/product-attribute-values")}>
+        Атрибут продукта 2
+      </Backlink>
+      <PageHeader title="Создать новый атрибут продукта" />
+      <div>
+        <Card>
+          <CardTitle title={"Основная информация"} />
+          <div className={classes.mainCardInfo}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-gh-label">
+                  Атрибут продукта
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-gh-label"
+                  id="demo-simple-select"
+                  value={isCategories}
+                  onChange={(e) => {
+                    setIsCategories(e.target.value);
+                    setNewData((prev) => ({
+                      ...prev,
+                      productinventory: e.target.value,
+                    }));
+                  }}
+                >
+                  {inventors?.map(({ sku, id }) => (
+                    <MenuItem key={id} value={id}>
+                      {sku}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormSpacer />
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-gh-label">Тип продукта</InputLabel>
+                <Select
+                  labelId="demo-simple-gh-label"
+                  id="demo-simple-select"
+                  value={isCategories}
+                  onChange={(e) => {
+                    setIsCategories(e.target.value);
+                    setNewData((prev) => ({
+                      ...prev,
+                      attributevalues: e.target.value,
+                    }));
+                  }}
+                >
+                  {attributes?.map(({ attribute_value, id }) => (
+                    <MenuItem key={id} value={id}>
+                      {attribute_value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+        </Card>
+        <CardSpacer />
+        <Button
+          style={{ float: "right", marginTop: "10px", padding: "10px 70px" }}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Сохранить
+        </Button>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductDetailsPageEdit;
